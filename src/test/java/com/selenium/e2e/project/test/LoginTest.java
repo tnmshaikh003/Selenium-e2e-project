ad4e49c2245387f9220d6650bd5a0c2b56334ce8
tnmshaikh003 <tanmoyshikh03@gmail.com>
2026-08-05 21:18:33 +0530

    added log

--- src/test/java/com/selenium/e2e/project/test/LoginTest.java
+++ src/test/java/com/selenium/e2e/project/test/LoginTest.java
@@ -2,6 +2,9 @@ package com.selenium.e2e.project.test;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 
 import org.testng.annotations.Test;
@@ -10,6 +13,8 @@ import com.selenium.e2e.project.base.BaseTest;
 import com.selenium.e2e.project.page.LoginPage;
 
 public class LoginTest extends BaseTest{
+
+    private static final Logger logger = LogManager.getLogger(LoginTest.class);
 
     public LoginTest() throws IOException {
         super();
@@ -20,9 +25,13 @@ public class LoginTest extends BaseTest{
     public void loginTest() {
         LoginPage lp = new LoginPage(driver.get());
         Assert.assertTrue(lp.navigateToSwagLab());
+        logger.log(Level.INFO,"Succesfully navigated to Swag lab");
         lp.enterUserName(prop.getProperty("userName"));
+        logger.log(Level.INFO,"Succesfully entered username");
         lp.enterPassword(prop.getProperty("password"));
+        logger.log(Level.INFO,"Succesfully entered password");
        Assert.assertTrue( lp.clickOnLogIn().isHeaderDisplayed());
+       logger.log(Level.INFO,"Succesfully veried header text");
         
     }
    
